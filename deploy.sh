38b489127172634b64e7eb422722f1e487f1c23a
Jonathan <jo.christianto@gmail.com>
2020-09-22 14:20:42 +0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:jochristianto/jochristianto.github.io.git master
 
 # if you are deploying to https://jochristianto.github.io/he-tree-vue
-# git push -f git@github.com:jochristianto/he-tree-vue.git master:gh-pages
+git push -f git@github.com:jochristianto/he-tree-vue.git master:gh-pages
 
 cd -
